6f469706cae269cd2f8930f09b379db32d5e09ea
TheMaxium69 <mxm.tournier@gmail.com>
2025-02-15 23:22:36 +0100

    update

--- src/app/deck/deck.component.ts
+++ src/app/deck/deck.component.ts
@@ -1,17 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NavbarComponent} from "../_global/navbar/navbar.component";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
+import {CardComponent} from "./card/card.component";
 
 @Component({
   selector: 'app-deck',
   standalone: true,
   imports: [
     NavbarComponent,
-    NgForOf
+    NgForOf,
+    CardComponent,
+    NgIf
   ],
   templateUrl: './deck.component.html',
   styleUrl: './deck.component.css'
 })
-export class DeckComponent {
+export class DeckComponent implements OnInit {
+  pageWidth: number = 0;
+
+  ngOnInit() {
+    this.getPageWidth(); // Récupérer la largeur initiale de la page
+    window.addEventListener('resize', () => {
+      this.getPageWidth(); // Mettre à jour la largeur lorsque la page est redimensionnée
+    });
+  }
+
+  getPageWidth() {
+    this.pageWidth = window.innerWidth;
+    console.log(this.pageWidth);
+  }
 
 }
