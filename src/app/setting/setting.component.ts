38a865fefdc3c663347c1c3f4167b6684aa31724
TheMaxium69 <mxm.tournier@gmail.com>
2025-03-05 18:45:59 +0100

    update

--- src/app/setting/setting.component.ts
+++ src/app/setting/setting.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import {AppComponent} from "../app.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-setting',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './setting.component.html',
   styleUrl: './setting.component.css'
 })
